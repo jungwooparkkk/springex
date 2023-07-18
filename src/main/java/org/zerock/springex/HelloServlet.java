be325457d09a53edd7386e0721cda773f95616e0
Jungwooparkkk <pjw7935@naver.com>
2023-07-18 20:32:23 +0900

    HelloServlet 회원가입 기능 구현

--- src/main/java/org/zerock/springex/HelloServlet.java
+++ src/main/java/org/zerock/springex/HelloServlet.java
@@ -25,10 +25,6 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
-
-        // commit
-        // commit22
-        // commit33
     }
 
     public void destroy() {
